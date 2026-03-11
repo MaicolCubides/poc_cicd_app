23faddb31dad011c2f8cd85c5620a6d1c4ecaa8f
Maicol Alexis Cubides Peña <macubides@iegrupo.local>
2026-03-11 11:01:11 -0500

    v2

--- src/core/config.py
+++ src/core/config.py
@@ -10,3 +10,13 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+
+
+# SI de casualidad debemos leer variables en pleto Pipeline de CI, debemos colocarlo asi:
+# from pydantic_settings import BaseSettings
+
+# class Settings(BaseSettings):
+#     DATABASE_URL: str
+#     ENV: str
+
+# settings = Settings()
